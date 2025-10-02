daf014ae88255be6eb575498e548edbe58a40f75
Sakthipriya07 <166027450+Sakthipriya07@users.noreply.github.com>
2025-10-02 10:40:03 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Currently, all accordion functionality is handled by Bootstrap’s JS.
+// If you want to add custom FAQ or Support Section interactivity, put it here.
+
+// Example: Console message on page load
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("FAQ & Support Page Loaded");
+});
